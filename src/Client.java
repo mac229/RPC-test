6344476d49b66abcf63c40a35a66587af8616c0e
Maciej Kozłowski <mac229@interia.eu>
2016-10-26 22:59:40 +0200

    Added client

---
+++ src/Client.java
@@ -0,0 +1,36 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+import com.googlecode.jsonrpc4j.JsonRpcServer;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by maciej on 26.10.16.
+ */
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        BasicConfigurator.configure();
+        JsonRpcServer server = new JsonRpcServer(new ObjectMapper(), new UserServiceImpl(), UserService.class);
+
+        JsonRpcHttpClient client = null;
+        try {
+            URL serviceUrl = new URL("http://127.0.0.1:1234");
+            client = new JsonRpcHttpClient(serviceUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try {
+            User user = client.invoke("createUser", new Object[]{"bob", "the builder", "password"}, User.class);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.googlecode.jsonrpc4j.JsonRpcServer;
+import com.googlecode.jsonrpc4j.StreamServer;
 import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+
+import javax.net.ServerSocketFactory;
+import javax.servlet.Servlet;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 
 public class Main {
 
-    static Logger logger = Logger.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         BasicConfigurator.configure();
         JsonRpcServer server = new JsonRpcServer(new ObjectMapper(), new UserServiceImpl(), UserService.class);
-        server.
+
+        Servlet servlet = new UserServiceServlet();
+        //servlet.init();
+
+        // create the stream server
+        int maxThreads = 50;
+        int port = 1420;
+        InetAddress bindAddress = InetAddress.getByName("localhost");
+        //Service client = ProxyUtil.createClientProxy(this.getClass().getClassLoader(), Service.class, jsonRpcClient, socket);
+        ServerSocket socket = ServerSocketFactory.getDefault().createServerSocket(1234, 0, InetAddress.getByName("localhost"));
+        StreamServer streamServer = new StreamServer(server, maxThreads, socket);
+
+        // start it, this method doesn't block
+        streamServer.start();
     }
 }
